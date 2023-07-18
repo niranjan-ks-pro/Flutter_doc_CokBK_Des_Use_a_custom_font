24e3b213d9aa5a6f43669989b41a445bb0375f08
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-07-18 11:20:37 +0530

    commit

---
+++ use_a_custom_font/README.md
@@ -0,0 +1,5 @@
+# use_a_custom_font
+
+A new Flutter project.
+
+"its a font project ignoring this time"
---
+++ use_a_custom_font/android/app/src/main/kotlin/com/example/use_a_custom_font/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.use_a_custom_font
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
